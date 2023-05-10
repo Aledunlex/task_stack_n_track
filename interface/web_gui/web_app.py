72c9ef2995096cdcd47778d9061f9048ec348ba7
Aledunlex <89398664+Aledunlex@users.noreply.github.com>
2023-05-10 15:58:30 +0000

    ajout du squelette non fonctionnel de qq fonctionnalités sur la web app
    * boutons de filtrage par catégorie
    * ajout d'une carte pour ajouter un nouvel élément

--- interface/web_gui/web_app.py
+++ interface/web_gui/web_app.py
@@ -4,28 +4,49 @@ import threading
 from flask import request
 
 from db import db_handler as db
-
+from model.element import Element, Quest
+from interface.displayable import Category
 
 
 def web_main(elems_by_categ: dict):
   app = Flask(__name__)
 
   def get_all_elements():
-    return [element for elements in elems_by_categ.values() for element in elements]
+    return [
+      element for elements in elems_by_categ.values() for element in elements
+    ]
 
+  def create_blank_element(element_class, element_attributes):
+    blank_element = element_class(**element_attributes, category="Category")
+    blank_element.category.background_color = "#ffffff"
+    return blank_element
 
+  
   def get_element_by_title_and_category(title, category_value):
     for elements in elems_by_categ.values():
       for element in elements:
         if element.title.value == title and element.category.value == category_value:
-          
+
           return element
     return None
 
   @app.route('/')
   def index():
-    all_elements = get_all_elements()
-    return render_template('index.html', all_elements=all_elements)
+      all_elements = get_all_elements()
+      categories = list(set([element.category for element in all_elements]))
+  
+      if all_elements:
+          element_class = type(all_elements[0])
+          element_attributes = {attr: attr.capitalize() for attr in all_elements[0].get_displayable_attributes()}
+      else:
+          element_class = Element
+          element_attributes = {}
+  
+      blank_element = create_blank_element(element_class, element_attributes)
+      all_elements.insert(0, blank_element)
+  
+      return render_template('index.html', all_elements=all_elements, categories=categories)
+
 
   @app.route('/update_done', methods=['POST'])
   def update_done():
@@ -35,7 +56,7 @@ def web_main(elems_by_categ: dict):
     element = get_element_by_title_and_category(element_title,
                                                 element_category)
     if element:
-      
+
       db.update_element_check(element, is_done)
       return "OK"
     else:
